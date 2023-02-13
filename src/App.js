b46c779c6ce256f2959f2ad9796c9b63c24eca1a
anom35 <freelance35830@gmail.com>
2023-02-13 17:43:19 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner"
 import Cart from "./components/Cart"
+import ShoppingList from "./components/ShopingList";
 
 function App() {
   return (
     <div>
         <Banner />
         <Cart />
+        <ShoppingList />
     </div>
   )
 }

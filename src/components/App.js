5c20d2d7ac93badfbb7ee394721657243602fdf5
anom35 <freelance35830@gmail.com>
2023-02-14 09:00:59 +0100

    commit OK

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import Banner from './Banner'
-import logo from './assets/logo.png'
+import logo from '../assets/logo.png'
 // import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'

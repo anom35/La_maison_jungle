cd80ecbd69b5f4f4a4382b30545cd6f06ea1e36c
anom35 <freelance35830@gmail.com>
2023-02-15 14:51:43 +0100

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,21 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
-// import Cart from './Cart'
+import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
 
 function App() {
-
-	// function handleSubmit(e) {
-	// 	e.preventDefault()
-	// 	alert(e.target['my_input'].value)
-	// }
-	
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-
-			{/* <Cart /> */}
-			<ShoppingList />
-
-			{/* <form onSubmit={handleSubmit}>
-				<input type='text' name='my_input' defaultValue='Tapez votre texte' />
-				<button type='submit'>Entrer</button>
-			</form> */}
-
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
 			<Footer />
 		</div>
 	)
